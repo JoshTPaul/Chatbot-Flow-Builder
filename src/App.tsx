5cf62fdb0c37d801dc8293373f76bb890f7ddc4f
Joshua Paul <joshpauljtp@gmail.com>
2024-04-28 16:21:14 +0530

    Switching to Zustand

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,17 @@ import "reactflow/dist/style.css";
 import Canvas from "./components/Canvas";
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
-import { FlowProvider } from "./components/useFlowBuilder";
 import "./index.css";
 
 const App = () => {
   return (
-    <FlowProvider>
+    <>
       <Header />
       <main>
         <Canvas />
         <Sidebar />
       </main>
-    </FlowProvider>
+    </>
   );
 };
 
